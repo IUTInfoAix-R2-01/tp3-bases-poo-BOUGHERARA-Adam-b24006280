6d3b04c37725c2e487bdc3e6da020ed7ec1a2583
Bougherara Adam <adam.bougherara@etu.univ-amu.fr>
2025-02-04 16:59:06 +0100

    Finalisation de la classe date

--- src/Date.java
+++ src/Date.java
@@ -10,9 +10,37 @@ public class Date {
 		this.year = year;
 	}
 	
-	public Date(int day,int month,int year) {
+	public int getDay() {
+		return this.day;
+	}
+	
+	public int getMonth() {
+		return this.month;
+	}
+	
+	public int getYear() {
+		return this.year;
+	}
+	
+	public void setDay(int day) {
+		 this.day = day;
+		}
+	
+	public void setMonth(int month) {
+	 this.month = month;
+	}
+	
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	public void setDate(int day, int month, int year) {
 		this.day = day;
 		this.month = month;
 		this.year = year;
+	}
+	
+	public String toString() {
+		return String.format("%02d/%02d/%04d",this.month,this.day, this.year);
 	}
 }
